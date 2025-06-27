9f02ee63aca09d1675fa065c4467e585b9fad824
Jamarius Mario Thorpe <mariothorpe22@gmail.com>
2025-06-27 14:35:50 -0400

    feat:
    - created login and sign up componenets and pages
    
    - updated the about page for my app
    
    - updated the NavBar to include links to login and sign up
    
    - created two more tables within the same base to be able to handle login and sign up requests

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
     return (
         <div>
             <p>
-                This is a roller coaster react app created for Code The Dream React course. This app allows coaster enthusiasts to search a database i've created, of roller coasters by a variety of categories. Users will then be able to add those roller coasters to their own personalized list. I hope you all enjoy and ride on!
+                This is a roller coaster react app created for Code The Dream React course. This app allows coaster enthusiasts to search a database that i've created with airtable, of roller coasters by a variety of categories. Users will then be able to create an account and add roller coasters to their own personalized list. Whether you're a seasoned coaster enthusiast or brand new to the world of roller coasters, this app is a great way to track coaster credits. I hope you all enjoy and ride on!
             </p>
         </div>
     );
